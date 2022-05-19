071b9dc71d582c761458c8944b61b6abb907df80
Lizandro G.Britto <Lizandro7u7@gmail.com>
2022-05-18 23:34:06 -0400

    Creacion de la plantilla del formulario

---
+++ resources/views/formulario.blade.php
@@ -0,0 +1,54 @@
+<html lang="es">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Formulario</title>
+  </head>
+  <body>
+
+<fieldset>
+  <legend>Comercio electronico en tiempos de cuarentena inteligente a causa del covid 19</legend>
+
+
+  <form action="form.php" method="post">
+    <p>a) Pregunta de ejemplo</p>
+      <p>Respuesta a <input type="radio" name="rta1" value="Respuesta_a_1"/p>
+      <p>Respuesta b <input type="radio" name="rta1" value="Respuesta_a_2"/p>
+      <p>Respuesta c <input type="radio" name="rta1" value="Respuesta_a_3"/p>
+      <p>Respuesta d <input type="radio" name="rta1" value="Respuesta_a_4"/p>
+      <p>Otro <input type="radio" name="rta1" value="Otro_1"/p>
+
+    <p>b) Pregunta de ejemplo</p>
+        <p>Respuesta a <input type="radio" name="rta2" value="Respuesta_b_2"/p>
+        <p>Respuesta b <input type="radio" name="rta2" value="Respuesta_b_2"/p>
+        <p>Respuesta c <input type="radio" name="rta2" value="Respuesta_b_2"/p>
+        <p>Respuesta d <input type="radio" name="rta2" value="Respuesta_b_2"/p>
+        <p>Otro <input type="radio" name="rta3" value="Otro_2"/p>
+
+    <p>c) Pregunta de ejemplo</p>
+        <p>Respuesta a <input type="radio" name="rta3" value="Respuesta_c_3"/p>
+        <p>Respuesta b <input type="radio" name="rta3" value="Respuesta_c_3"/p>
+        <p>Respuesta c <input type="radio" name="rta3" value="Respuesta_c_3"/p>
+        <p>Respuesta d <input type="radio" name="rta3" value="Respuesta_c_3"/p>
+        <p>Otro <input type="radio" name="rta3" value="Otro_3"/p>
+
+    <p>d) Pregunta de ejemplo</p>
+        <p>Respuesta a <input type="radio" name="rta4" value="Respuesta_d_4"/p>
+        <p>Respuesta b <input type="radio" name="rta4" value="Respuesta_d_4"/p>
+        <p>Respuesta c <input type="radio" name="rta4" value="Respuesta_d_4"/p>
+        <p>Respuesta d <input type="radio" name="rta4" value="Respuesta_d_4"/p>
+        <p>Otro <input type="radio" name="rta4" value="Otro_4"/p>
+
+    <p><input type="submit" name="enviar" value="Enviar"> </p>
+  </form>
+
+</fieldset>
+
+
+  </body>
+</html>
