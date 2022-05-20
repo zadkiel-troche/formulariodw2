09207d0f3777423e72630b4da921243f1cc32fde
JonhatanOwO <oznekodesuke@gmail.com>
2022-05-20 12:35:23 -0400

    minnor changes

--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -9,34 +9,40 @@
 
     <title>Formulario</title>
   </head>
-  <body>
+  <body style="background-color:#DEF5E3;">
+  <div class="container-fluid shadow-lg" style="padding: 20px; background-color:#326C3C; color:white">
+  <div class="row">
+  <div class="col-1"></div>
+  <div class="col-2"><img src="Logo.png" alt="Logo" width="60px"></div>
+  <div style="text-aling-left; font-size:25px;" class="col-9">Comercio electronico en tiempos de cuarentena inteligente a causa del covid 19</div>
+  </div>
+  </div>
+  <div style="background-color:#F8F9F9; padding: 30px;" class="Container-md shadow">
 
-  <div style="background-color:#dedbd9; ">
-    <h3>Comercio electronico en tiempos de cuarentena inteligente a causa del covid 19</h3>
-
-
-    <form  action="./form-enviar" method="post">
+      <div class="row">
+            <div class="col-sm-3"></div>
+    <form  action="./form-enviar" method="post" class="col-sm-6">
       {{csrf_field()}}
       <p>a) ¿Implementaba el e-commerce en su empresa antes de la pandemia?</p>
             <input type="text" class="form-control" name="rta1" required>
       <p>b) ¿Le costo adaptarse al modelo del e-commerce?</p>
             <input type="text" class="form-control" name="rta2" required>
-      <p>c) ¿Que metodos de pago acepto durante la pandemia?</p>
+      <p>c) ¿Que métodos de pago acepto durante la pandemia?</p>
             <input type="text" class="form-control" name="rta3" required>
-      <p>d) ¿Encontro problemas a la hora de implementar el modelo de e-commerce?</p>
+      <p>d) ¿Encontró problemas a la hora de implementar el modelo de e-commerce?</p>
             <input type="text" class="form-control" name="rta4" required>
       <p>e) ¿Como promociono la empresa durante la pandemia? </p>
             <input type="text" class="form-control" name="rta5" required>
-      <p>f) ¿Considera util el e-commerce actualmente?</p>
-            <input type="text" class="form-control" name="rta6" required>
+      <p>f) ¿Considera útil el e-commerce actualmente?</p>
+            <input type="text" class="form-control" name="rta6" required><br>
 
 
 
 
-      <input type="submit" class="btn btn-primary form-control" name="enviar" value="Enviar">
-
+      <input type="submit" class="btn btn-dark form-control" name="enviar" value="Enviar">
+      <div class="col-sm-3"></div>
       </form>
-
+      </div>
     </div>
 
   </body>
